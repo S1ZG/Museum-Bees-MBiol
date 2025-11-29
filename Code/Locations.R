fceb5fd5e37fdad0b8cde617be4253e22d25c9b7
S1ZG <Inaya@Ny-MBP.local>
2025-11-29 15:25:08 +0000

    Commit updated locations

--- Code/Locations.R
+++ Code/Locations.R
@@ -55,56 +55,16 @@ na_early_data_geo <- early_data_geo %>% filter(is.na(latitude) & !is.na(location
 # save early_data_geo as a .csv
 write.csv(early_data_geo, "Data/early_data_geocoded.csv", row.names = FALSE)
 
+# I then manually entered the lat long coordinates for the NA columns using google maps
+# Add the lat long back to the original early_data dataframe
 
 
 
 
-#Attempt to use local nature reserve data
-############
+# Set up a workflow for when I have updated early_data 
+# To save processing I will add the lat long to the newest dataframe, then isolate the unique locations that do not have a lat long coordinate
 
 
-# read local shapefile (download from Natural England or Protected Planet)
-local_nature_reserves <- read_sf("Data/Local_Nature_Reserves_(England)___Natural_England.shp")
-
-
-library(stringr)
-library(fuzzyjoin)
-
-
-
-# st_crs(local_nature_reserves)               # see CRS (important)
-# names(local_nature_reserves)                # find a human-readable name field (e.g., "NAME", "SiteName")
-# print sample rows
-local_nature_reserves %>% select(1:6) %>% head()
-
-
-
-
-# 1) make cleaned reserve name
-local_nature_reserves <- local_nature_reserves %>%
-  mutate(
-    NAME_clean = tolower(str_squish(NAME))
-  )
-
-# 2) make a point-on-surface for each polygon and set that as the active geometry
-#    (this ensures the geometry column contains POINTs, not polygons)
-reserves_pts <- local_nature_reserves %>%
-  mutate(rep_point = st_point_on_surface(geometry)) %>%  # create the point geometry
-  st_set_geometry("rep_point")                           # replace active geometry with the point
-
-
-# 3) transform these point geometries to WGS84
-reserves_pts_wgs84 <- st_transform(reserves_pts, crs = 4326)
-
-# 4) extract lon/lat (one row per feature)
-coords <- st_coordinates(reserves_pts_wgs84)            # will be n x 2 (X, Y)
-reserves_pts_wgs84$longitude <- coords[,1]
-reserves_pts_wgs84$latitude  <- coords[,2]
-
-# 5) keep only desired columns as a plain data.frame for joining
-reserve_points_small <- reserves_pts_wgs84 %>%
-  st_set_geometry(NULL) %>%
-  select(NAME_clean, latitude, longitude)
 
 
 
