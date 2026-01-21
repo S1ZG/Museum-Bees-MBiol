ae2a80295ea12200ca5f32834c70a40aae615d39
S1ZG <Inaya@Ny-MBP.local>
2026-01-21 18:44:02 +0000

    Make plans for temp code

---
+++ Code/Temperature.R
@@ -0,0 +1,24 @@
+
+# This code is for getting temperature data for the locations of the museum bee specimens
+
+# Load required libraries
+library(tidyverse)
+library(sf)
+library(terra)
+library(tidyterra)
+library(exactectractr)
+
+
+# Read in measurement data with geocoded locations
+
+
+
+# To see if vice county resolution is sufficient, check the differences between monthly means for different counties
+# If they are small enough, we can use vice county means
+
+# Get temperature data
+
+
+
+
+# Match locations to temperatures
