28a62317178e20382e6036db8f52d91c3fe16464
S1ZG <shil6029@ox.ac.uk>
2026-03-21 15:34:50 +0000

    LMs attempt

--- Code/LMs_analysis1.R
+++ Code/LMs_analysis1.R
@@ -2,6 +2,7 @@
 # This code is for running LMs and AIC analyses
 
 library(ggplot2)
+library(dplyr)
 
 
 bees_temps <- read_csv(here("Data/17_03_26_bees_temps_5km.csv"))
@@ -9,19 +10,40 @@ bees_temps <- read_csv(here("Data/17_03_26_bees_temps_5km.csv"))
 bees_temps <- bees_temps[bees_temps$label_no != "IGproject0099" & bees_temps$label_no != "IGproject0232", ]
 
 
-# Shorten measurement names
+# Shorten measurement names and rename full_name to sp
 bees_temps <- bees_temps %>%
   rename(
     HW = HW_mm,
     ITD = intertegular_distance_mm,
     FW = FW_length_mm,
-    tibia = tibia_length_mm
+    tibia = tibia_length_mm,
+    sp = full_name
+  )
+
+bees_temps <- bees_temps %>%
+  mutate(
+    log_ITD = log(ITD),
+    log_HW  = log(HW),
+    log_FW = log(FW),
+    log_tibia = log(tibia)
   )
 
 
 # Rescale year
 bees_temps$year_rescaled <- (bees_temps$year - 1800) / 100
 # Scaled to "centuries since 1800" to improve model stability
+
+
+model1 <- lm(log_ITD ~ year_rescaled + mean_preflight_temp + sex + sp, data = bees_temps)
+
+summary(model1)
+
+library(lme4)
+model2 <- lmer(log_ITD ~ year_rescaled + mean_preflight_temp + sex +
+                 (1 | sp),
+               data = bees_temps)
+
+summary(model2)
 
 
 
